f47fdb0ba8b132e026e81e610fe38a768f585ea7
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2021-05-21 10:33:47 -0400

    Store arguments of events for deferred events.

--- src/Event/RobloxEvent.lua
+++ src/Event/RobloxEvent.lua
@@ -9,6 +9,8 @@ local CLASS_NAME = "RobloxEvent"
 
 
 
+local HttpService = game:GetService("HttpService")
+
 local NexusObjectFolder = script.Parent.Parent
 local NexusObject = require(NexusObjectFolder:WaitForChild("NexusObject"))
 local NexusEvent = require(script.Parent:WaitForChild("NexusEvent"))
@@ -27,6 +29,15 @@ function RobloxEvent:__new()
     self:InitializeSuper()
     self.Connections = {}
     self.BindableEvent = Instance.new("BindableEvent")
+
+    --For deferred events, the arguments need to be stored.
+    --LastArgumentsStrong will keep the reference around and prevent
+    --it from being garbage collected until only LastArguments references it.
+    --Ideally, they will be used at the same time if both :Connect() and
+    --:Wait() are used.
+    self.LastArgumentsStrong = {}
+    self.LastArguments = {}
+    setmetatable(self.LastArguments,{__mode="v"})
 end
 
 --[[
@@ -66,15 +77,20 @@ the event is raised.
 function RobloxEvent:Connect(Function)
     --Create the connection.
     local Connection = NexusConnection.new(self,Function)
-    
+
     --Set up the bindable event.
-    local BindableEventConnection = self.BindableEvent.Event:Connect(function()
-        Connection:Fire(unpack(self.LastArguments,1,self.TotalLastArguements))
+    local BindableEventConnection = self.BindableEvent.Event:Connect(function(UUID)
+        --Get the arguments.
+        local Arguments = self.LastArguments[UUID]
+        self.LastArgumentsStrong[Arguments] = nil
+
+        --Fire the event.
+        Connection:Fire(unpack(Arguments.Arguments,1,Arguments.Total))
     end)
-    
+
     --Store the connections.
     self.Connections[Connection] = BindableEventConnection
-    
+
     --Return the connection.
     return Connection
 end
@@ -84,9 +100,17 @@ end
 Fires the event.
 --]]
 function RobloxEvent:Fire(...)
-    self.LastArguments = {...}
-    self.TotalLastArguements = select("#",...)
-    self.BindableEvent:Fire()
+    --Store the arguments.
+    local UUID = HttpService:GenerateGUID()
+    local Arguments = {
+        Arguments = {...},
+        Total = select("#",...),
+    }
+    self.LastArgumentsStrong[UUID] = Arguments
+    self.LastArguments[UUID] = Arguments
+
+    --Invoke the event.
+    self.BindableEvent:Fire(UUID)
 end
 
 --[[
@@ -94,8 +118,13 @@ Yields the current thread until this signal
 is fired. Returns what was fired to the signal.
 --]]
 function RobloxEvent:Wait()
-    self.BindableEvent.Event:Wait()
-    return unpack(self.LastArguments,1,self.TotalLastArguements)
+    --Wait for the event.
+    local UUID = self.BindableEvent.Event:Wait()
+
+    --Return the arguments.
+    local Arguments = self.LastArguments[UUID]
+    self.LastArgumentsStrong[Arguments] = nil
+    return unpack(Arguments.Arguments,1,Arguments.Total)
 end
 
 
